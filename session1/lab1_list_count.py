ad09fb8b8e19b0b787b49ce7c231f90116fede72
Moatasem El Sayed <eng.moatasem.8@gmail.com>
2025-06-19 17:40:32 +0200

    add session 1

---
+++ session1/lab1_list_count.py
@@ -0,0 +1,14 @@
+"""Write a Python program to count the number 4 in a given list."""
+
+
+def count(lst):
+    """Write your solution here. Don't forget to return the result at the end."""
+
+
+if __name__ == "__main__":
+    assert count([1, 2, 3, 4, 5, 4, 6]) == 2, "Test case failed"
+    assert count([1, 2, 3, 5, 6]) == 0, "Test case failed"
+    assert count([4, 4, 4, 4]) == 4, "Test case failed"
+    assert count([]) == 0, "Test case failed"
+    assert count([4, 5, 6, 7, 8]) == 1, "Test case failed"
+    assert count([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 1, "Test case failed"
---
+++ session1/lab5_accumulator.py
@@ -0,0 +1,16 @@
+"""Sum 1 … n — Compute and display the running total."""
+
+
+def compute(number):
+    """write your soulution Here Sum 1 … n — Compute and display the running total."""
+
+
+if __name__ == "__main__":
+    assert compute(5) == 15, "Test case failed"
+    assert compute(10) == 55, "Test case failed"
+    assert compute(100) == 5050, "Test case failed"
+    assert compute(0) == 0, "Test case failed"
+    assert compute(1) == 1, "Test case failed"
+    assert compute(2) == 3, "Test case failed"
+    assert compute(3) == 6, "Test case failed"
+    assert compute(4) == 10, "Test case failed"
